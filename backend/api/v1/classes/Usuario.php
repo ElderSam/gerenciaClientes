d144ec24222d1f4894e64afdcb4031fe7d981ba5
ElderSam <eldersamuel98@gmail.com>
2020-08-16 08:50:38 -0300

    Usuários: listar (GET)

--- backend/api/v1/classes/Usuario.php
+++ backend/api/v1/classes/Usuario.php
@@ -10,6 +10,11 @@ class Usuario
 {
     public function list()
     {        
+        $sql = new Sql();
 
+        $query = "SELECT * FROM usuarios";
+        $res = $sql->select($query);
+
+        echo json_encode($res);
     }
 }
